7323b0abf4f54d771b17988c6e4b4fc355f07030
Geff115 <gabrielnoah129@gmail.com>
2024-07-31 08:01:24 +0100

    Setting up web servers for the deployment of web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,18 +3,28 @@
 # install nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
-# make file structure
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-# add simple content
-echo "Testing my nginx configuration" >> /data/web_static/releases/test/index.html
-# symbolic link
-ln -fs /data/web_static/releases/test/ /data/web_static/current
-# give ownership to user and group
-chown -R ubuntu:ubuntu /data
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static 
-sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+
+# Make required directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Add simple content to the test index.html file
+echo "Testing my nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '/listen 80 default_server;/a \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# Allow Nginx through the firewall
 sudo ufw allow "Nginx HTTP"
+
+# Restart Nginx to apply the changes
 sudo service nginx restart
-exit 1
+
+# Exit successfully
+exit 0
