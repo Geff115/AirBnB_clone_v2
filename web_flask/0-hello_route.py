9d4d5b4f90225f387d6d245481209d129a8efe62
Geff115 <gabrielnoah129@gmail.com>
2024-07-31 06:43:00 +0100

    A script that starts a Flask web application listening on port 5000

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application listening on port 5000
+"""
+
+from . import app
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    This function returns Hello HBNB!
+    """
+
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/__init__.py
@@ -0,0 +1,10 @@
+"""
+Initialization file that registers a Flask application
+and perform setup needed for the package.
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
